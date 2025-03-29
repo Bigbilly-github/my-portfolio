44997bfa3441661a934c4168d7518e7ae87db5c1
Bigbilly-github <abiolatwitter97@gmail.com>
2025-03-29 10:11:54 +0100

    changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,7 +21,7 @@ function Header (){
             <h1 className="font-bebas w-[128px] h-[28px]  xl:w-[146px] xl:h-[48px] font-normal text-[28px] xl:text-[32px] leading-[150%] -tracking-[1%]  text-[#C7C7C7]">YUSUF ABIOLA</h1>
             <nav className="w-[130px]  h-[24px]  flex items-center justify-between ">
                
-                <a onClick={scrollToPositionContact} className="w-[110px]  h-[24px] hover:opacity-65 cursor-pointer font-inter font-medium text-[18px] sm:text-[18px] mr-[20px] leading-[150%]  -tracking-[3%] text-[#C7C7C7]" >Contact Me</a>
+                <a onClick={scrollToPositionContact} className="w-[110px] active:bg-black active:text-black h-[24px] hover:opacity-65 cursor-pointer font-inter font-medium text-[18px] sm:text-[18px] mr-[20px] leading-[150%]  -tracking-[3%] text-[#C7C7C7]" >Contact Me</a>
 
                
             </nav>
